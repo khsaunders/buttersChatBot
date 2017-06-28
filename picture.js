e0c80e059b5e0284ed44f833901471dc89e70d9f
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2017-06-28 18:02:06 -0400

    Add more lines to picture command

--- picture.js
+++ picture.js
@@ -6,7 +6,8 @@ $('#submit').on('click', function userPicRequest(){
   if (picCheck > -1){
     var time = 500;
       setTimeout(function buttersSendsPic(){
-        var buttersPicWords = "Oh you want a picture? Okay, let me get my good side";
+        var buttersLines = ['Oh you want a picture? Okay, let me get my good side', 'Mom says I am handsome', 'You should follow my instagram: butters.chillin', 'Ready for my closeup'];
+        var buttersPicWords = buttersLines[Math.floor(Math.random()*buttersLines.length)];
         var buttersPicLi = document.createElement('LI');
         var chatList = document.getElementById('chatList');
 
@@ -14,7 +15,7 @@ $('#submit').on('click', function userPicRequest(){
         chatList.append(buttersPicLi);
         buttersPicLi.classList.add('buttersResponse');
 
-        buttersPics = ['drButterscotch.jpg', 'IMG_0200.jpg', 'IMG_4272.jpg', 'IMG_4697.jpg', 'IMG_5430.jpg', 'IMG_5755.jpg'];
+        buttersPics = ['drButterscotch.jpg', 'IMG_0200.jpg', 'IMG_4272.jpg', 'IMG_5430.jpg', 'IMG_5755.jpg'];
         var pictureName = buttersPics[Math.floor(Math.random()*buttersPics.length)];
         var picAppend = document.createElement('img');
           picAppend.setAttribute("src", "images/" + pictureName);
